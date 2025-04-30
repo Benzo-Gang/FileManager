3947cbe41d962ea8997448f75bc0f7cc31eb8f3b
Vit0GG <135022604+Vit0GG@users.noreply.github.com>
2025-05-01 00:05:21 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace file_manager
+{
+    partial class GoBack2
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.GoOver = new System.Windows.Forms.Button();
+            this.GoBack = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(541, 388);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(371, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // GoOver
+            // 
+            this.GoOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GoOver.Location = new System.Drawing.Point(474, 11);
+            this.GoOver.Name = "GoOver";
+            this.GoOver.Size = new System.Drawing.Size(79, 23);
+            this.GoOver.TabIndex = 2;
+            this.GoOver.Text = "Перейти";
+            this.GoOver.UseVisualStyleBackColor = true;
+            this.GoOver.Click += new System.EventHandler(this.GoOver_Click);
+            // 
+            // GoBack
+            // 
+            this.GoBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GoBack.Location = new System.Drawing.Point(389, 11);
+            this.GoBack.Name = "GoBack";
+            this.GoBack.Size = new System.Drawing.Size(79, 23);
+            this.GoBack.TabIndex = 3;
+            this.GoBack.Text = "Назад";
+            this.GoBack.UseVisualStyleBackColor = true;
+            this.GoBack.Click += new System.EventHandler(this.GoBack_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // GoBack2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 479);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.GoBack);
+            this.Controls.Add(this.GoOver);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "GoBack2";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GoBack2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button GoOver;
+        private System.Windows.Forms.Button GoBack;
+        private System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace file_manager
+{
+    public partial class GoBack2 : Form
+    {
+
+        public GoBack2()
+        {
+            InitializeComponent();
+            InitializeContextMenu();
+            
+        }
+
+        public void InitializeContextMenu()
+        {
+            
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+           
+            ToolStripMenuItem copyItem = new ToolStripMenuItem("Копировать", null, CopyTo_Click);
+            ToolStripMenuItem pasteItem = new ToolStripMenuItem("Вставить", null, MoveTo_Click);
+            ToolStripMenuItem deleteItem = new ToolStripMenuItem("Удалить", null, Delete_Click);
+            ToolStripMenuItem renameItem = new ToolStripMenuItem("Переименовать", null, RenameIt_Click);
+            ToolStripMenuItem archiveItem = new ToolStripMenuItem("Архивировать", null, GZipStream_Click);
+
+            contextMenu.Items.AddRange(new ToolStripItem[] { copyItem, pasteItem, deleteItem, renameItem, archiveItem });
+
+            listBox1.ContextMenuStrip = contextMenu;
+        }
+        public void GoOver_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            DirectoryInfo Dir = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo[] Dirs = Dir.GetDirectories();
+            foreach (DirectoryInfo CurrentDir in Dirs)
+            {
+                listBox1.Items.Add(CurrentDir.FullName);
+
+            }
+
+            FileInfo[] files = Dir.GetFiles();
+
+            foreach (FileInfo CurrentFile in files)
+            {
+                listBox1.Items.Add(CurrentFile.FullName);
+            }
+
+        }
+
+        public void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (Path.GetExtension(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())) == "")
+            {
+                textBox1.Text = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString());
+                listBox1.Items.Clear();
+
+                DirectoryInfo Dir = new DirectoryInfo(textBox1.Text);
+                DirectoryInfo[] Dirs = Dir.GetDirectories();
+                foreach (DirectoryInfo CurrentDir in Dirs)
+                {
+                    listBox1.Items.Add(CurrentDir.FullName);
+
+                }
+
+                FileInfo[] files = Dir.GetFiles();
+
+                foreach (FileInfo CurrentFile in files)
+                {
+                    listBox1.Items.Add(CurrentFile.FullName);
+                }
+            }
+            else
+            {
+                Process.Start(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString()));
+            }
+
+
+        }
+
+        public void GoBack_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text[textBox1.Text.Length - 1] == '\\')
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                }
+            }
+
+            else if (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                }
+            }
+
+            listBox1.Items.Clear();
+
+            DirectoryInfo Dir = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo[] Dirs = Dir.GetDirectories();
+            foreach (DirectoryInfo CurrentDir in Dirs)
+            {
+                listBox1.Items.Add(CurrentDir.FullName);
+
+            }
+
+            FileInfo[] files = Dir.GetFiles();
+
+            foreach (FileInfo CurrentFile in files)
+            {
+                listBox1.Items.Add(CurrentFile.FullName);
+            }
+        }
+        
+
+        public void listBox1_Click(object sender, EventArgs e)
+        {
+            listBox1.Focus();
+        }
+
+
+
+        public void Delete_Click(object sender, EventArgs e)
+        {
+            DeleteItem();
+        }
+
+        public void CopyTo_Click(object sender, EventArgs e)
+        {
+           CopyItem();
+        }
+
+
+        public static class Prompt
+        {
+            public static string ShowDialog(string text, string caption)
+            {
+                Form prompt = new Form()
+                {
+                    Width = 400,
+                    Height = 200,
+                    Text = caption
+                };
+
+                Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+                System.Windows.Forms.TextBox textBox = new System.Windows.Forms.TextBox() { Left = 50, Top = 50, Width = 300 };
+                System.Windows.Forms.Button myButton = new System.Windows.Forms.Button() { Text = "ОК", Left = 250, Width = 100, Top = 100 };
+                myButton.Click += (sender, e) => { prompt.Close(); };
+
+                prompt.Controls.Add(textLabel);
+                prompt.Controls.Add(textBox);
+                prompt.Controls.Add(myButton);
+
+                prompt.ShowDialog();
+                return textBox.Text;
+            }
+        }
+
+
+        public void MoveTo_Click(object sender, EventArgs e)
+        {
+            PasteItem();
+            
+        }
+
+        public void RenameIt_Click(object sender, EventArgs e)
+        {
+           RenameItem();
+        }
+
+        public void GZipStream_Click(object sender, EventArgs e)
+        {
+            ArchiveItem();
+        }
+    
+
+    public void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady) 
+                {
+                    comboBox1.Items.Add(drive.Name);
+                }
+            }
+        }
+        public void LoadDirectoriesAndFiles()
+        {
+            listBox1.Items.Clear(); 
+
+            DirectoryInfo Dir = new DirectoryInfo(textBox1.Text);
+
+            DirectoryInfo[] Dirs = Dir.GetDirectories();
+            foreach (DirectoryInfo CurrentDir in Dirs)
+            {
+                listBox1.Items.Add(CurrentDir.FullName);
+            }
+
+            FileInfo[] files = Dir.GetFiles();
+            foreach (FileInfo CurrentFile in files)
+            {
+                listBox1.Items.Add(CurrentFile.FullName);
+            }
+        }
+
+        public void GoBack2_Load(object sender, EventArgs e)
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady) 
+                {
+                    comboBox1.Items.Add(drive.Name);
+                }
+            }
+        }
+
+        public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.SelectedItem.ToString();
+            LoadDirectoriesAndFiles();
+        }
+
+        public string copiedPath;
+        public void CopyItem()
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                copiedPath = listBox1.SelectedItem.ToString();
+                MessageBox.Show("Элемент скопирован: " + copiedPath);
+            }
+        }
+        public void PasteItem()
+        {
+            if (!string.IsNullOrEmpty(copiedPath))
+            {
+                string NextPath = textBox1.Text;
+                string itemName = Path.GetFileName(copiedPath);
+                string FullPath = Path.Combine(NextPath, itemName);
+
+                if (Directory.Exists(copiedPath))
+                {
+                    DirectoryCopy(copiedPath, FullPath, true);
+                }
+                else
+                {
+                    File.Copy(copiedPath, FullPath, true);
+                }
+
+                MessageBox.Show("Элемент вставлен: " + FullPath);
+                GoOver_Click(null, null);
+            }
+        }
+
+        public void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException("Источник не существует: " + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+
+
+
+        public void HandleException(Exception ex)
+        {
+            if (ex is UnauthorizedAccessException)
+            {
+                MessageBox.Show("Ошибка: У вас нет прав для выполнения этой операции.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (ex is IOException)
+            {
+                MessageBox.Show("Ошибка: Произошла ошибка ввода-вывода. " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Неизвестная ошибка: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void DeleteItem()
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string pathToDelete = listBox1.SelectedItem.ToString();
+                if (MessageBox.Show("Вы уверены, что хотите удалить " + pathToDelete + "?", "Подтверждение", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        if (Directory.Exists(pathToDelete))
+                        {
+                            Directory.Delete(pathToDelete, true);
+                        }
+                        else
+                        {
+                            File.Delete(pathToDelete);
+                        }
+                        MessageBox.Show("Элемент удален: " + pathToDelete);
+                        GoOver_Click(null, null);
+                    }
+                    catch (Exception ex)
+                    {
+                        HandleException(ex);
+                    }
+                }
+            }
+        }
+
+        public void RenameItem()
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string oldPath = listBox1.SelectedItem.ToString();
+                string newName = Prompt.ShowDialog("Введите новое имя:", "Переименовать");
+
+                if (!string.IsNullOrEmpty(newName))
+                {
+                    string newPath = Path.Combine(Path.GetDirectoryName(oldPath), newName);
+                    try
+                    {
+                        if (Directory.Exists(oldPath))
+                        {
+                            Directory.Move(oldPath, newPath);
+                        }
+                        else
+                        {
+                            File.Move(oldPath, newPath);
+                        }
+                        MessageBox.Show("Элемент переименован в: " + newPath);
+                        GoOver_Click(null, null);
+                    }
+                    catch (Exception ex)
+                    {
+                        HandleException(ex);
+                    }
+                }
+            }
+        }
+
+        public void ArchiveItem()
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string pathToArchive = listBox1.SelectedItem.ToString();
+                string archivePath = pathToArchive + ".zip";
+
+                try
+                {
+                    if (Directory.Exists(pathToArchive))
+                    {
+                        ZipFile.CreateFromDirectory(pathToArchive, archivePath);
+                    }
+                    else if (File.Exists(pathToArchive))
+                    {
+                        using (FileStream fs = new FileStream(archivePath, FileMode.Create))
+                        using (ZipArchive archive = new ZipArchive(fs, ZipArchiveMode.Create))
+                        {
+                            archive.CreateEntryFromFile(pathToArchive, Path.GetFileName(pathToArchive));
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Указанный путь не существует.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    MessageBox.Show("Элемент заархивирован: " + archivePath);
+                }
+                catch (Exception ex)
+                {
+                    HandleException(ex);
+                }
+            }
+        }
+    }
+}
+//ошибки и комбобокс
+ 
