978617a8d66667ee3c9e66e9487191d686f34c61
Vit0GG <mishatupchi@gmail.com>
2025-05-02 13:25:56 +0300

    Add ListView and change ComboBox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,24 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.GoOver = new System.Windows.Forms.Button();
-            this.GoBack = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listViewBooks = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 72);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(541, 388);
-            this.listBox1.TabIndex = 0;
-            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
-            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
             // 
             // textBox1
             // 
@@ -58,24 +50,13 @@
             // GoOver
             // 
             this.GoOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.GoOver.Location = new System.Drawing.Point(474, 11);
+            this.GoOver.Location = new System.Drawing.Point(389, 11);
             this.GoOver.Name = "GoOver";
             this.GoOver.Size = new System.Drawing.Size(79, 23);
             this.GoOver.TabIndex = 2;
             this.GoOver.Text = "Перейти";
             this.GoOver.UseVisualStyleBackColor = true;
             this.GoOver.Click += new System.EventHandler(this.GoOver_Click);
-            // 
-            // GoBack
-            // 
-            this.GoBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.GoBack.Location = new System.Drawing.Point(389, 11);
-            this.GoBack.Name = "GoBack";
-            this.GoBack.Size = new System.Drawing.Size(79, 23);
-            this.GoBack.TabIndex = 3;
-            this.GoBack.Text = "Назад";
-            this.GoBack.UseVisualStyleBackColor = true;
-            this.GoBack.Click += new System.EventHandler(this.GoBack_Click);
             // 
             // comboBox1
             // 
@@ -84,18 +65,57 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 13;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listViewBooks
+            // 
+            this.listViewBooks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listViewBooks.FullRowSelect = true;
+            this.listViewBooks.HideSelection = false;
+            this.listViewBooks.Location = new System.Drawing.Point(12, 67);
+            this.listViewBooks.Name = "listViewBooks";
+            this.listViewBooks.Size = new System.Drawing.Size(541, 400);
+            this.listViewBooks.TabIndex = 14;
+            this.listViewBooks.UseCompatibleStateImageBehavior = false;
+            this.listViewBooks.View = System.Windows.Forms.View.Details;
+            this.listViewBooks.ItemActivate += new System.EventHandler(this.listViewBooks_ItemActivate);
+            this.listViewBooks.SelectedIndexChanged += new System.EventHandler(this.listViewBooks_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Заголовок";
+            this.columnHeader1.Width = 70;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Автор";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Оценка";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Дата выхода";
+            this.columnHeader4.Width = 90;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "цена";
             // 
             // GoBack2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(572, 479);
+            this.Controls.Add(this.listViewBooks);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.GoBack);
             this.Controls.Add(this.GoOver);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
             this.Name = "GoBack2";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.GoBack2_Load);
@@ -106,10 +126,15 @@
 
         #endregion
         private System.Windows.Forms.Button GoOver;
-        private System.Windows.Forms.Button GoBack;
         private System.Windows.Forms.ComboBox comboBox1;
-        public System.Windows.Forms.ListBox listBox1;
         public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listViewBooks;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
     }
 }
+
 
